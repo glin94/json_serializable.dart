d3960c577724825f6769f7733dcdc03ffd4b379f
Linar Giniyatov <glinar94@mail.ru>
2022-11-16 12:12:36 +0000

    chore

--- json_serializable/lib/src/utils.dart
+++ json_serializable/lib/src/utils.dart
@@ -183,6 +183,7 @@ extension StringExtension on String {
   String capitalize() {
     return '${this[0].toUpperCase()}${substring(1).toLowerCase()}';
   }
+}
 
 String encodedFieldName(
   FieldRename fieldRename,
